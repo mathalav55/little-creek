91862e4ac74724d70f590edb7e7c4691b4d5aa73
mathalav55 <matalav55@gmail.com>
2022-07-31 19:47:57 +0530

    layout responsive draft-1

--- js/app.js
+++ js/app.js
@@ -1,17 +1,7 @@
-//Accordition
-var faqs = document.querySelectorAll('.faq');
-Array.from(faqs).forEach( faq =>{
-    var title = faq.querySelector('.title');
-    title.addEventListener('click',()=>{
-        //open accordition on click
-        title.querySelector('.icon').textContent = faq.classList.contains('hide') ? '-' : '+';
-        faq.classList.toggle('hide');
-        //close other active accorditions
-        Array.from(faqs).forEach( item =>{
-            if( faq != item && !item.classList.contains('hide')){
-                item.classList.add('hide');
-                item.querySelector('.icon').textContent="+";
-            }
-        });
-    });
-});
+var hamMenu  = document.querySelector('.ham-menu');
+var nav = document.querySelector('nav');
+var navList = nav.querySelector('ul');
+hamMenu.addEventListener('click',()=>{
+    hamMenu.classList.toggle('open');
+    navList.classList.toggle('active');
+})
