8c03fa1fc5f0fde71cf0a38a06d4cdd61e497533
mathalav55 <matalav55@gmail.com>
2022-08-02 10:09:35 +0530

    added accordition animation

--- js/app.js
+++ js/app.js
@@ -1,7 +1,18 @@
+// Ham menu
 var hamMenu  = document.querySelector('.ham-menu');
 var nav = document.querySelector('nav');
 var navList = nav.querySelector('ul');
 hamMenu.addEventListener('click',()=>{
     hamMenu.classList.toggle('open');
     navList.classList.toggle('active');
+})
+
+// Accordition
+var accorditionItems = document.querySelectorAll('.accordition-item');
+
+accorditionItems.forEach(item => {
+    var title = item.querySelector('.accordition-title');
+    title.onclick = ()=>{
+        item.classList.toggle('hide');
+    }
 })
