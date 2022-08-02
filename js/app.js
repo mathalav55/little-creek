22a91d65b8265f9e5d904a5de609045895d3cce6
mathalav55 <matalav55@gmail.com>
2022-08-02 21:01:24 +0530

    footer layout modified

--- js/app.js
+++ js/app.js
@@ -5,14 +5,19 @@ var navList = nav.querySelector('ul');
 hamMenu.addEventListener('click',()=>{
     hamMenu.classList.toggle('open');
     navList.classList.toggle('active');
-})
+});
 
 // Accordition
 var accorditionItems = document.querySelectorAll('.accordition-item');
-
 accorditionItems.forEach(item => {
     var title = item.querySelector('.accordition-title');
+    var titleIcon  = title.querySelector('span');
     title.onclick = ()=>{
         item.classList.toggle('hide');
+        if( item.classList.contains('hide')){
+            titleIcon.textContent = '+';
+        }else{
+            titleIcon.textContent = '-';
+        }
     }
-})
+});
